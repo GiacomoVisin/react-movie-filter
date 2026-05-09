cfb3f3bac52c421d14ec2ec79e0c0cbb40ddfe99
GiacomoVisin <killerboss1150@gmail.com>
2026-05-09 15:46:07 +0200

    filtering for genre added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
+import movies from "./assets/data/data"
+
 function App() {
+
+
+  //initial movies
+  const [InitialMovies, setInitialMovies] = useState(movies)
+
+  // for select the genre
+  const [selectedGenre, setSelectedGenre] = useState('');
+
+  //for the filtered movies
+  const [filteredMovies, setFilteredMovies] = useState(InitialMovies);
+
+
+  const genres = [...new Set (movies.map((movie)=> movie.genre))]
+
+  // for filtering of the movies
+  useEffect(() => {
+    let result = movies;
+      
+
+    if (selectedGenre !== '') {
+      result = result.filter(
+        (movie) => movie.genre === selectedGenre
+      );
+    } setFilteredMovies(result);
+  }, [movies, selectedGenre]);
+
 
 
   return (
     <>
+      <h1> Film List</h1>
+      <select
+        value={selectedGenre}
+        onChange={(e) => setSelectedGenre(e.target.value)}>
+           <option value={``}> Seleziona Genere </option>
+        {genres.map((genre,i) => (
+          <option key={i} value={genre}>
+            {genre}
+          </option>
+        ))}
+      </select>
 
+
+      <ul>
+        {filteredMovies.map((movie,i)=>(
+          <li key={i}> {movie.title}  </li>
+        ))}
+      </ul>
     </>
   )
 }
--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,12 +1,11 @@
-export default data
 
-const Films = [
+const movies = [
     { title: 'Inception', genre: 'Fantascienza' },
     { title: 'Il Padrino', genre: 'Thriller' },
     { title: 'Titanic', genre: 'Romantico' },
     { title: 'Batman', genre: 'Azione' },
     { title: 'Interstellar', genre: 'Fantascienza' },
     { title: 'Pulp Fiction', genre: 'Thriller' },
-]
+];
 
-
+export default movies;
