b828520208350ef2156da158c818b7931be34997
GiacomoVisin <killerboss1150@gmail.com>
2026-05-09 15:15:04 +0200

    adding js

---
+++ src/assets/data/data.js
@@ -0,0 +1,12 @@
+export default data
+
+const Films = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+
