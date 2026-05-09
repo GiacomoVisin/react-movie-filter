4fe5f44c9f14019ee3f3b37189f4fb49d419f4da
GiacomoVisin <killerboss1150@gmail.com>
2026-05-09 16:20:21 +0200

    adding a validation form for add new films

--- src/App.jsx
+++ src/App.jsx
@@ -16,26 +16,57 @@ function App() {
   // for filter the film by title
   const [searchTitle, setSearchTitle] = useState(``)
 
-  const genres = [...new Set(movies.map((movie) => movie.genre))]
+  function HandleAddMovie(e) {
+    e.preventDefault()
+
+     const movieExists = InitialMovies.some(
+    (movie) =>
+      movie.title.toLowerCase() === newTitleFilm.toLowerCase()
+  );
+
+  if (movieExists) {
+    alert("There is already a film with this title, please change it!");
+    return;
+  }
+    const newMovie = {
+      title: newTitleFilm,
+      genre: newGenreFilm
+    }
+    
+
+
+    setInitialMovies([...InitialMovies, newMovie])
+    setNewTitle('');
+    setNewGenre('');
+  }
+
+
+
+  const [newTitleFilm, setNewTitleFilm] = useState('');
+  const [newGenreFilm, setNewGenreFilm] = useState('');
+
+
+
+  const genres = [...new Set(InitialMovies.map((movie) => movie.genre))]
 
   // for filtering of the movies
   useEffect(() => {
-    let result = movies;
+    let result = InitialMovies;
 
 
     if (selectedGenre !== '') {
       result = result.filter(
         (movie) => movie.genre === selectedGenre
       );
-    } 
+    }
 
     if (searchTitle !== ``) {
       result = result.filter((movie) =>
         movie.title.toLowerCase().includes(searchTitle.toLowerCase())
       )
-    }setFilteredMovies(result);
+    } setFilteredMovies(result);
 
-  }, [movies, selectedGenre, searchTitle]);
+  }, [InitialMovies, selectedGenre, searchTitle]);
 
 
 
@@ -62,6 +93,12 @@ function App() {
           <li key={i}> {movie.title}  </li>
         ))}
       </ul>
+
+      <form onSubmit={HandleAddMovie}>
+        <input type="text" value={newTitleFilm} onChange={(e)=> setNewTitleFilm(e.target.value)}  placeholder="type name for new movie"/>
+        <input type="text" value={newGenreFilm} onChange={(e)=> setNewGenreFilm(e.target.value)}   placeholder="type genre for new movie"/>
+        <button> Add new Films</button>
+      </form>
     </>
   )
 }
